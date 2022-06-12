9a6c02d4f1ae46163d37a77768836f24824044fa
AnaJuBaAr <106597257+AnaJuBaAr@users.noreply.github.com>
2022-06-12 11:55:17 -0300

    Add files via upload

---
+++ D.S/Lista 02/at1.java
@@ -0,0 +1,9 @@
+public class at1 {
+    public static void main (String args[]){
+        int loop = 1;
+        while (loop<=50){
+            System.out.println(loop);
+            loop = loop + 1;
+        }
+    }
+}
---
+++ D.S/Lista 02/at2.java
@@ -0,0 +1,11 @@
+public class at2 {
+    public static void main (String args []){
+        int loop = 1;
+        while (loop <=100){
+            if(loop%2==0){
+                System.out.println(loop);
+            }
+            loop = loop + 1;
+        }
+    }
+}
