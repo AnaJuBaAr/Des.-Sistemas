755075e92d98fb64eb51a2a8eddc84c24ebf4c5c
AnaJuBaAr <106597257+AnaJuBaAr@users.noreply.github.com>
2022-08-19 14:03:07 -0300

    Add files via upload

---
+++ D.S/Lista 03/Questao02/Livro.java
@@ -0,0 +1,31 @@
+package Lista03.Questao02;
+
+public class Livro {
+    String nomeTitulo, nomeEditora, nomeAutor;
+    int pag, anoPublicacao;
+
+    String  titulo(String nomeDoLivro){
+        this.nomeTitulo = nomeDoLivro;
+        return nomeTitulo;
+    }
+
+    String editora (String nomeDaEditora){
+        this.nomeEditora = nomeDaEditora;
+        return nomeEditora;
+    }
+
+    String autor (String nomeDoAutor){
+        this.nomeAutor = nomeDoAutor;
+        return nomeAutor;
+    }
+
+    int paginas (int numeroDePaginas){
+        this.pag = numeroDePaginas;
+        return numeroDePaginas;
+    }
+
+    int publicacao (int anoDePublicacao){
+        this.anoPublicacao = anoDePublicacao;
+        return anoPublicacao;
+    }
+}
