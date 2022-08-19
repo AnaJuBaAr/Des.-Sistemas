7ca47e2999fade9696bf2b43b84940910b3ab6c7
AnaJuBaAr <106597257+AnaJuBaAr@users.noreply.github.com>
2022-08-19 14:44:44 -0300

    Create Questao12.java

---
+++ 2 Ano/D.S/Lista01/Questao12.java
@@ -0,0 +1,8 @@
+public class questao12 {
+    public static void main(String args []){
+        int km = 15;
+        int minseg = 50 * 60;
+        double posicaofinal = (km * minseg) / 1000;
+        System.out.println("Terminará à "+posicaofinal);
+    }
+}
