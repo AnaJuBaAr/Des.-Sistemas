c54ca199731688d91e52b2a410ccdf2b06b26a14
AnaJuBaAr <106597257+AnaJuBaAr@users.noreply.github.com>
2022-08-19 14:04:16 -0300

    Add files via upload

---
+++ D.S/Lista 03/Questao03/Autor.java
@@ -0,0 +1,13 @@
+package Lista03.Questao03;
+
+public class Autor {
+    String nom, data;
+
+    String nomeAutor (String nomeDA){
+        this.nom = nomeDA;
+        return nom;
+    }
+    void dataDeNascimento (String dataDN){
+        this.data = dataDN;
+    }
+}
---
+++ D.S/Lista 03/Questao03/Editora.java
@@ -0,0 +1,16 @@
+package Lista03.Questao03;
+
+public class Editora {
+    String nome, endereco, cnpj;
+
+    String nomeEditora (String nomeDE){
+        this.nome = nomeDE;
+        return nome;
+    }
+    void enderecoEditora (String enderecoDE){
+        this.endereco = enderecoDE;
+    }
+    void cnpjEmpresa (String cnpjDE){
+        this.cnpj = cnpjDE;
+    }
+}
