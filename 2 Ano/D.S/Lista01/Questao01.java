5f984487feab6482006dec75a5353200fbc5740b
AnaJuBaAr <106597257+AnaJuBaAr@users.noreply.github.com>
2022-08-19 14:35:01 -0300

    Update Questao01.java

--- 2 Ano/D.S/Lista01/Questao01.java
+++ 2 Ano/D.S/Lista01/Questao01.java
@@ -1,1 +1,17 @@
+package aula;
+import java.util.Scanner;
 
+public class Questao01 {
+    public static void main (String args []) {
+        Scanner input = new Scanner (System.in);
+        int numero1, numero2, total;
+
+        System.out.print("Informe um número: ");
+        numero1 = input.nextInt();
+        System.out.print("Informe um número: ");
+        numero2 = input.nextInt();
+        total = numero1 + numero2;
+        System.out.println("A soma dos valores é igual a "+total);
+        input.close();
+    }
+}
