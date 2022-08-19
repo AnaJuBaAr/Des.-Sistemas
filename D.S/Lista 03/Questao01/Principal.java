076c0a07b775d2a93152767b4d76d06037dccb50
AnaJuBaAr <106597257+AnaJuBaAr@users.noreply.github.com>
2022-08-19 14:02:07 -0300

    Add files via upload

--- D.S/Lista 03/Questao01/Principal.java
+++ D.S/Lista 03/Questao01/Principal.java
@@ -1,1 +1,27 @@
+package Lista03.Questao01;
 
+import java.util.Scanner;
+
+public class Principal {
+    public static void main (String args[]){
+        Scanner input = new Scanner(System.in);
+        Aluno anaJulia = new Aluno();
+        double P1, P2, P3, T;
+
+        System.out.println("Informe a nota:");
+        System.out.print("\nda 1° prova: ");
+        P1 = input.nextInt();
+        System.out.print("\nda 2° prova: ");
+        P2 = input.nextInt();
+        System.out.print("\nda 3° prova: ");
+        P3 = input.nextInt();
+        System.out.print("\ndo trabalho: ");
+        T = input.nextInt();
+
+        anaJulia.nota1(P1);
+        anaJulia.nota2(P2);
+        anaJulia.nota3(P3);
+        anaJulia.nota4(T);
+        System.out.println("Media final: "+anaJulia.consultarNotaFinal());
+    }
+}
