0992eedd629ededef4fa9a4255fafcb52f4b3abf
AnaJuBaAr <106597257+AnaJuBaAr@users.noreply.github.com>
2022-08-26 10:12:07 -0300

    Create Principal.java

---
+++ 2 Ano/D.S/Lista04/Questao05/Principal.java
@@ -0,0 +1,11 @@
+package Lista04.Questao05;
+
+public class Principal {
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+
+        carro.ligar();
+        carro.acelerar();
+        carro.desligar();
+    }
+}
