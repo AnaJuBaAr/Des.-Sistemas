804c660a62946c6f9facdf4f7cf3c271bb283d6b
AnaJuBaAr <106597257+AnaJuBaAr@users.noreply.github.com>
2022-08-26 08:32:26 -0300

    Delete at1.java

--- D.S/Lista 02/at1.java
+++
@@ -1,9 +0,0 @@
-public class at1 {
-    public static void main (String args[]){
-        int loop = 1;
-        while (loop<=50){
-            System.out.println(loop);
-            loop = loop + 1;
-        }
-    }
-}
