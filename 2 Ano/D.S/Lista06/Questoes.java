f6a91ac5dbfd8e825e03837de77cc04a87f063ac
AnaJuBaAr <106597257+AnaJuBaAr@users.noreply.github.com>
2022-10-19 20:08:51 -0300

    Create Questoes.java

---
+++ 2 Ano/D.S/Lista06/Questoes.java
@@ -0,0 +1,22 @@
+/*
+QUESTÃO 08:
+  É um tipo especial de classe que não pode ser instanciada, somente herdada. Sendo assim, não possui objetos.
+  A principal diferença entre os dois é que uma interface é considerada um contrato, tendo que ser assinado pelas classes. Além disso, uma classe
+abstrata é herdada, enquanto uma interface é implementada.
+
+QUESTÃO 09:
+  Sim. Porém uma classe abstrata só é PURA quando não possui métodos concretos.
+  
+QUESTÃO 10:
+  Sim. Não é obrigatório que uma classe abstrata tenha métodos abstratos.
+  
+QUESTÃO 11:
+  Ela serve para que as classes filhas não precisem se preocupar com comportamentos padrões, somente com suas particularidades.
+
+QUESTÃO 12:
+  Construtores são, por padrão, métodos concretos, o que não é permitido em uma interface.
+  
+QUESTÃO 13:
+  As classes abstratas são usadas de modelo para as classes filhas. Se um de seus métodos é privado, ele não pode ser acessado pelas subclasses, o que 
+vai contra o propósito de sua criação.
+*/
