18d123864814df36dcb24ea16dc06944368d6b37
AnaJuBaAr <106597257+AnaJuBaAr@users.noreply.github.com>
2022-10-20 10:53:22 -0300

    Add files via upload

--- 2 Ano/D.S/Lista06/Questao16/Lavavel.java
+++ 2 Ano/D.S/Lista06/Questao16/Lavavel.java
@@ -1,1 +1,10 @@
-
+public interface Lavavel {
+    public void lavar(){
+        System.out.println("Lavando...");
+    }
+    /*métodos presentes em interfaces, não podem ter corpo.
+    *
+    * O jeito correto de se fazer esse método seria:
+    * */
+    public void lavar2();
+}
