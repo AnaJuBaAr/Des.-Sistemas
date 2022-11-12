61277a76b266b1bd779bbd446db222071e6424ba
AnaJuBaAr <106597257+AnaJuBaAr@users.noreply.github.com>
2022-11-12 20:30:20 -0300

    Update EnderecoDAO.java

--- 2 Ano/D.S/Projeto/IFEstagio/IFEstagio/src/ifestagio/dao/EnderecoDAO.java
+++ 2 Ano/D.S/Projeto/IFEstagio/IFEstagio/src/ifestagio/dao/EnderecoDAO.java
@@ -1,9 +1,11 @@
 package ifestagio.dao;
 
 import ifestagio.model.Endereco;
+import java.util.List;
 
 public interface EnderecoDAO {
     void salvar(Endereco endereco);
     Endereco get(long idEndereco);
     void remover(Endereco endereco);
+    List<Endereco>getTodos();
 }
