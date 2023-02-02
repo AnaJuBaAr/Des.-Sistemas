61ba4661eafc8c3d5b2138c325de76a70a48daab
AnaJuBaAr <106597257+AnaJuBaAr@users.noreply.github.com>
2023-02-02 15:43:47 -0300

    Create Dados.sql

---
+++ 2 Ano/B.D/MySQL/Biblioteca/Dados.sql
@@ -0,0 +1,28 @@
+insert into Autor values (1, 'Machado de Assis'),
+                          (2,'Paulo Coelho'),
+                          (3, 'Monteiro Lobato');
+INSERT INTO Autor VALUES(4, 'Beth OLeary'),
+	(5, 'Laini Taylor'),
+	(6, 'Neil Gaiman'),
+	(7, 'Maggie Stiefvater'),
+	(8, 'Ailton Krenak'),
+	(9, 'Ernest Cline'),
+	(10, 'Elphinstone Dayrell'),
+	(11, 'Giambattista'),
+	(12, 'Villamaria Basile'),
+	(13, 'Wish');
+                         
+insert into Livro values (100, 'Dom Casmurro', '1920-01-15', 29.90, 1);
+                         (199, 'Helena', '1930-11-30', 39.90, 1),
+                         (200, 'O Alienista', '1931-05-10', 59.50, 1),
+                         (201, 'As mil e uma noites', '1750-01-01', 100.01, null);
+INSERT INTO Livro VALUES('1115789012341', 'Teto para dois', '2019-04-10', 34.90, 1),
+	('2225789056782', 'Um estranho sonhador', '2017-03-28', 54.90, 2),
+	('3335789090123', 'Mitologia Nórdicas', '2017-02-07', 42.99, 3),
+	('4445789010114', 'Os garotos corvos', '2012-09-01', 36.90, 4),
+	('5555789012135', 'O amanhã não está a venda', '2020-04-18', 2.90, 5),
+	('6665789014156', 'Coraline', '2002-07-02', 42.04, 3),
+	('7775789016177', 'As mil e uma noites - Livro 1', '0001-01-01', 35.90, null),
+	('8885789018198', 'O livro dos mortos do antigo egito', '0001-01-01', 47.72, null),
+	('9995789020219', 'Epopeia de Gilgamesh', '0001-01-01', 45.99, null),
+	('0005789022230', 'Bíblia', '0001-01-01', 54.90, null);
